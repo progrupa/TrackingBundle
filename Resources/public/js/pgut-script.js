7780ca2cec84f3c7b726892cec5c7324c2c1b8e9
Radek Przybysz <radoslaw@przybysz.net.pl>
2026-03-23 13:03:35 +0100

    FIX infinite refresh on FF v4

--- Resources/public/js/pgut-script.js
+++ Resources/public/js/pgut-script.js
@@ -2,29 +2,32 @@
 (function() {
     'use strict';
 
-    const LS = window.localStorage;
-    const UT = window._pgut;
-    const TX = {
+    var LS = window.localStorage;
+    var UT = window._pgut;
+    var TXT = {
         loop: 'pgut-redirect-loop',
         date: 'pgut-refresh-date'
     };
+    var redirecting = false;
 
     function messageReceived (e) {
-        let data;
+        var data;
         try { data = JSON.parse(e.data); } catch (err) { return; }
-        if (typeof data !== 'object' || typeof data.event !== 'string' || typeof data.data === 'undefined') { return; }
+        if (data === null || typeof data !== 'object' || typeof data.event !== 'string' || typeof data.data === 'undefined') { return; }
 
         if (typeof LS !== 'undefined') {
             if (data.event === 'pgTrackingSaved') { /** Tracking was saved, no need to do it again, do not include the iframe anymore */
-            const now = new Date();
-                LS.setItem(TX.date, new Date(now.setDate(now.getDate() + 365)).toString());
+                var now = new Date();
+                LS.setItem(TXT.date, new Date(now.setDate(now.getDate() + 365)).toString());
+                LS.removeItem(TXT.loop);
             }
 
-            if (data.event === 'pgSetCookie') {
-                let loop = LS.getItem(TX.loop) ? parseInt(LS.getItem(TX.loop), 10) : 0;
+            if (data.event === 'pgSetCookie' && !redirecting) {
+                var loop = LS.getItem(TXT.loop) ? parseInt(LS.getItem(TXT.loop), 10) : 0;
                 if (loop < 2) { /** max two refresh */
-                const ref = (typeof UT.referer !== 'undefined') ? ('?referer='+ UT.referer) : '';
-                    LS.setItem(TX.loop, (++loop).toString());
+                    redirecting = true;
+                    var ref = (typeof UT.referer !== 'undefined') ? ('?referer='+ UT.referer) : '';
+                    LS.setItem(TXT.loop, (++loop).toString());
                     window.location.href = UT.base +'pgut-set'+ ref;
                 }
             }
@@ -33,17 +36,18 @@
 
     function appendIframe () {
         if (typeof UT.hash !== 'undefined') {
-            const $frame = document.createElement('iframe');
+            var $frame = document.createElement('iframe');
             $frame.style.cssText = 'display:none !important;width:1px !important;height:1px !important;opacity:0 !important;pointer-events:none !important;';
             $frame.src = UT.base +'pgut-store/'+ UT.id +'/'+ UT.hash;
-            const $body = document.body;
+            var $body = document.body;
 
             if (typeof LS !== 'undefined') {
-                const future = LS.getItem(TX.date);
+                var future = LS.getItem(TXT.date);
                 if (typeof future !== 'undefined' && future !== null) {
-                    const now = new Date();
+                    var now = new Date();
                     if (now.getTime() >= new Date(future).getTime()) {
-                        LS.removeItem(TX.loop);
+                        LS.removeItem(TXT.loop);
+                        LS.removeItem(TXT.date);
                         $body.appendChild($frame);
                     }
                 } else {
@@ -55,8 +59,8 @@
         }
     }
 
-    const method = window.addEventListener ? 'addEventListener' : 'attachEvent';
-    const trigger = window[method];
+    var method = window.addEventListener ? 'addEventListener' : 'attachEvent';
+    var trigger = window[method];
     trigger(((method === 'attachEvent') ? 'onload' : 'load'), appendIframe, false); /** Append iframe to body */
     trigger(((method === 'attachEvent') ? 'onmessage' : 'message'), messageReceived, false); /** Listen to message from child window */
 })(window);
